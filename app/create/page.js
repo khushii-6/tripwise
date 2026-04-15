f48fa97fcb985bee96ddf610ecf2c4201a9f22c4
khushii-6 <khusisiingh66@gmail.com>
2026-04-16 00:22:31 +0530

    Create page.js

---
+++ app/create/page.js
@@ -0,0 +1,8 @@
+<div style={{
+  maxWidth: "700px",
+  margin: "40px auto",
+  background: "rgba(255,255,255,0.05)",
+  padding: "25px",
+  borderRadius: "20px",
+  backdropFilter: "blur(10px)"
+}}>
